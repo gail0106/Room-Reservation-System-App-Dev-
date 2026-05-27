4e0aa39c27d967cf83099fedb9f888fd484426cf
gail0106 <edmiebenjie@gmail.com>
2026-05-27 18:22:50 +0800

    Update settings.py

--- myproject/backend/config/settings.py
+++ myproject/backend/config/settings.py
@@ -11,8 +11,11 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
-from dotenv import load_dotenv
-load_dotenv()
+try:
+    from dotenv import load_dotenv
+    load_dotenv()
+except ImportError:
+    pass
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
